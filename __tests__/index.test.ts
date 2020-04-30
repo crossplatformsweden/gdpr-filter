cf72653e5a09f2e157053d254e613b6f5d63fc1b
Emil Apelgren <emilapelgren@outlook.com>
2020-04-30 10:55:07 +0200

    added lint

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,25 +1,23 @@
-
-import { filter } from '..'
+import { filter } from '..';
 
 describe('filter', () => {
-    it('should remove properties', () => {
-        const obj = {
-            a: "1",
-            ssn: "2",
-            personummer: "3",
-            nested: {
-                code: "",
-                b: ""
-            }
-        }
-        const filtered = filter(obj)
+  it('should remove properties', () => {
+    const obj = {
+      a: '1',
+      ssn: '2',
+      personummer: '3',
+      nested: {
+        code: '',
+        b: '',
+      },
+    };
+    const filtered = filter(obj);
 
-        expect(filtered).toEqual({
-            a: "1",
-            nested: {
-                b: ""
-            }
-        })
-
+    expect(filtered).toEqual({
+      a: '1',
+      nested: {
+        b: '',
+      },
     });
+  });
 });
--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 
+// ESLINT TODOS
 const removeKey = (object: any, searchString: string) => {
-    for(let prop in object) {
-      //console.log(prop)
-      if (prop.includes(searchString)) {
-        delete object[prop];
-      }
-      else if (typeof object[prop] === 'object')
-        removeKey(object[prop], searchString);
-    }
+  // eslint-disable-next-line no-restricted-syntax
+  for (const prop in object) {
+    if (prop.includes(searchString)) {
+      // eslint-disable-next-line no-param-reassign
+      delete object[prop];
+    } else if (typeof object[prop] === 'object') removeKey(object[prop], searchString);
   }
+};
 
-const list = ["pass", "code", "ssn", "social","birth", "fodelse", "födelse", "person", "pnr"]
+const list = ['pass', 'code', 'ssn', 'social', 'birth', 'fodelse', 'födelse', 'person', 'pnr'];
 
+// eslint-disable-next-line import/prefer-default-export
 export const filter = (object: any) => {
-    list.forEach(c => removeKey(object, c))
-    console.log(object)
-    return {
-        ...object
-    }
-}
+  list.forEach((c) => removeKey(object, c));
+  return {
+    ...object,
+  };
+};
