8b2805e54317d624b0110d3721fcc27d5cb0e26e
Emil Apelgren <emilapelgren@outlook.com>
2021-12-03 09:51:29 +0000

    upgrade

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 
 const removeKey = (object: any, searchString: string) => {
-    for(let prop in object) {
-      if (prop.includes(searchString)) {
-        delete object[prop];
-      }
-      else if (typeof object[prop] === 'object')
-        removeKey(object[prop], searchString);
+  for (let prop in object) {
+    if (prop.includes(searchString)) {
+      delete object[prop];
     }
+    else if (typeof object[prop] === 'object')
+      removeKey(object[prop], searchString);
   }
+}
 
-const list = ["pass", "code", "ssn", "social","birth", "fodelse", "födelse", "person", "pnr", "phone", "email"]
+const list = ["pass", "code", "ssn", "social", "birth", "fodelse", "födelse", "person", "pnr", "phone", "email"]
 
-export const filter = (object: any) => {
-    list.forEach(c => removeKey(object, c))
-    return {
-        ...object
-    }
+export const gdprfilter = (object: any) => {
+  list.forEach(c => removeKey(object, c))
+  return {
+    ...object
+  }
 }
